fcb9ae3b79d916c0e73df6398946bd0625ab08de
aivahnenko <aivahnenko866@gmail.com>
2026-02-26 09:30:35 +0300

    4.py

--- 4.py
+++ 4.py
@@ -1,11 +1,11 @@
 ip = input('Введите ваш IP-адрес: ')
 parts = ip.split('.')
 if len(parts) != 4:
-    print("НЕТ")
+    print('НЕТ')
 else:
     for part in parts:
         if not (part.isdigit() and 0 <= int(part) <= 255):
-            print("НЕТ")
+            print('НЕТ')
             break
     else:
-        print("ДА")
+        print('ДА')
